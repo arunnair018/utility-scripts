8bfaadc4a01ab54cdc53500d368ef4c0c5a2b9a3
SantaHub <pbaustin96@gmail.com>
2017-10-27 19:32:22 +0530

    Git knowledge

---
+++ git.sh
@@ -0,0 +1,12 @@
+git
+
+git init # Create git repo locally
+git add file.ext 
+git add *
+
+git commit file.ext -m "Single file commit"
+git commit -a -m "ALl commit "
+
+git remote add origin https://github.com/SantaHub/CoDS.git
+git remote set-url --push origin https://github.com/arunnair018/misc.git
+
